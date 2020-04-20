c2140172bec9c8737f81b03a1905222931434f3f
joseph <joseph.siyi@gmail.com>
2020-04-21 01:45:10 +0800

    Text

--- lib/demo/basic_demo.dart
+++ lib/demo/basic_demo.dart
@@ -3,15 +3,23 @@ import 'package:flutter/material.dart';
 class BasicDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final _textStyle = TextStyle(
+      fontSize: 20.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.black87,
+    );
+
+    final String _author = 'Libai';
+    final String _title = 'JiangJinJiu';
+
     return Center(
       child: Text(
-          'hello',
+          ' 《$_title》----$_author。君不见黄河之水天上来，奔流到海不复回。君不见高堂明镜悲白发，朝如青丝暮成雪。人生得意须尽欢，莫使金樽空对月。天生我材必有用，千金散尽还复来。烹羊宰牛且为乐，会须一饮三百杯。',
           textDirection: TextDirection.ltr,
-          style: TextStyle(
-            fontSize: 40.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.black87,
-          )
+          textAlign: TextAlign.right,
+          maxLines: 3,
+          overflow: TextOverflow.ellipsis,
+          style: _textStyle
       ),
     );
   }
