d25b644ebacf5eb07b78d6bef8ce4421f4cf31e4
joseph <joseph.siyi@gmail.com>
2020-04-20 11:41:57 +0800

    水波纹效果

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ninghao_flutter/demo/listview_demo.dart';
+import 'package:ninghao_flutter/demo/hello_demo.dart';
 
 void main() => runApp(App());
 
@@ -7,10 +8,13 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
       debugShowCheckedModeBanner: false,
       home: Home(),
       theme: ThemeData(
-        primarySwatch: Colors.yellow
+        primarySwatch: Colors.yellow,
+        highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
+        splashColor: Colors.white70
       )
     );
   }
@@ -19,30 +23,50 @@ class App extends StatelessWidget {
 class Home extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
-      return Scaffold(
-        backgroundColor: Colors.grey[100],
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: "Navigation",
-            onPressed: () => debugPrint("Navigation button is pressed!"),
-          ),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.search),
-              tooltip: "Search1 ",
-              onPressed: () => debugPrint("Search1 button is pressed!"),
+      return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+            backgroundColor: Colors.grey[100],
+            appBar: AppBar(
+              leading: IconButton(
+                icon: Icon(Icons.menu),
+                tooltip: "Navigation",
+                onPressed: () => debugPrint("Navigation button is pressed!"),
+              ),
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.search),
+                  tooltip: "Search1 ",
+                  onPressed: () => debugPrint("Search1 button is pressed!"),
+                ),
+                IconButton(
+                  icon: Icon(Icons.accessibility),
+                  tooltip: "Search2 ",
+                  onPressed: () => debugPrint("Search2 button is pressed!"),
+                )
+              ],
+              title: Text('NINGHAO'),
+              bottom: TabBar(
+                unselectedLabelColor: Colors.black38,
+                indicatorColor: Colors.black54,
+                indicatorSize: TabBarIndicatorSize.label,
+                indicatorWeight: 1,
+                tabs: <Widget>[
+                  Tab(icon: Icon(Icons.local_florist)),
+                  Tab(icon: Icon(Icons.change_history)),
+                  Tab(icon: Icon(Icons.directions_bike))
+                ],
+              ),
+              elevation: 0.0,
             ),
-            IconButton(
-              icon: Icon(Icons.accessibility),
-              tooltip: "Search2 ",
-              onPressed: () => debugPrint("Search2 button is pressed!"),
+            body: TabBarView(
+              children: <Widget>[
+                Hello(),
+                ListViewDemo(),
+                Hello()
+              ],
             )
-          ],
-          title: Text('NINGHAO'),
-          elevation: 0.0,
         ),
-        body: ListViewDemo()
       );
     }
 }
